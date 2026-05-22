53603cda446cab75d517422bf0ef33bb15d6c9de
Antonio Curavalea <one.kyonblack@gmail.com>
2026-05-22 21:31:36 +0300

    ci: add Vercel build script

---
+++ .vercel/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
+curl -sSL https://dioxus.dev/install.sh | bash
+/vercel/.cargo/bin/dx bundle --platform web --debug-symbols=false --out-dir bundle
