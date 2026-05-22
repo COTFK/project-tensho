fd77404dc97755c0bc4e2fa8669a0de8be5d7202
Antonio Curavalea <one.kyonblack@gmail.com>
2026-05-22 21:33:49 +0300

    fix(ci): point to correct DX CLI path

--- .vercel/build.sh
+++ .vercel/build.sh
@@ -3,4 +3,4 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "/rust/env"
 curl -sSL https://dioxus.dev/install.sh | bash
-/vercel/.cargo/bin/dx bundle --platform web --debug-symbols=false --out-dir bundle
+/rust/bin/dx bundle --platform web --debug-symbols=false --out-dir bundle
