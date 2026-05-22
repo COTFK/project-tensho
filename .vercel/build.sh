a85ddb7ceb138ea96672309cb44c468c7024a38f
Antonio Curavalea <one.kyonblack@gmail.com>
2026-05-22 21:38:52 +0300

    ci: give up on Vercel

--- .vercel/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source "/rust/env"
-curl -sSL https://dioxus.dev/install.sh | bash
-/rust/bin/dx bundle --platform web --debug-symbols=false --out-dir bundle
