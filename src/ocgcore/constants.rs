b83f9ebbb0e5274c06c7d9c071dbc4d4423e8143
Antonio Curavalea <one.kyonblack@gmail.com>
2026-05-26 18:41:07 +0300

    feat: implement `TryFrom<u32>` for `BattlePosition`

--- src/ocgcore/constants.rs
+++ src/ocgcore/constants.rs
@@ -83,6 +83,19 @@ impl TryFrom<u8> for BattlePosition {
         }
     }
 }
+impl TryFrom<u32> for BattlePosition {
+    type Error = anyhow::Error;
+
+    fn try_from(value: u32) -> anyhow::Result<Self, Self::Error> {
+        match value {
+            1 => Ok(BattlePosition::FaceUpAttack),
+            2 => Ok(BattlePosition::FaceDownAttack),
+            4 => Ok(BattlePosition::FaceUpDefense),
+            8 => Ok(BattlePosition::FaceDownDefense),
+            _ => anyhow::bail!("Received wrong battle position value: {value}"),
+        }
+    }
+}
 
 impl Display for BattlePosition {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
