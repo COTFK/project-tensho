2c5b8d1101a8da57ce240cbf1051983ba6a716c8
Antonio Curavalea <one.kyonblack@gmail.com>
2026-06-03 23:24:25 +0300

    feat: raise ATK/DEF font weight on small screens

--- src/ui/components/card.rs
+++ src/ui/components/card.rs
@@ -34,7 +34,7 @@ pub fn Card(
                 class: if !show_blue_aura && !show_orange_aura {"hidden"}
             }
             div {
-                class: "absolute bottom-0 right-0 text-white text-[6px] md:text-sm font-semibold z-5 bg-black pt-1 pl-1 pr-0.5 rounded-tl-xl",
+                class: "absolute bottom-0 right-0 text-white text-[6px] md:text-sm font-bold md:font-semibold z-5 bg-black pt-1 pl-1 pr-0.5 rounded-tl-xl",
                 class: if !show_stats || !card.card_type.contains(CardType::MONSTER) {"hidden"},
                 {format!("{}/{}", card.attack.unwrap_or(0), card.defense.unwrap_or(0))}
             }
