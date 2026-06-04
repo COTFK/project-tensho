e6e82085c5fd95566d012d95129518e18c3e4334
Antonio Curavalea <one.kyonblack@gmail.com>
2026-06-04 15:34:35 +0300

    feat: adjust stats for all screens

--- src/ui/field.rs
+++ src/ui/field.rs
@@ -85,12 +85,12 @@ fn Zone(index: u8, location: CardLocation, card: Option<CardData>) -> Element {
                     card
                 }
                 div {
-                    class: "absolute top-0 left-0 text-white text-[8px] md:text-sm font-semibold z-5 bg-gray-900/75 pl-0.5 pr-1 pb-0.5 rounded-br-lg",
+                    class: "absolute top-0 left-0 text-white text-[8px] md:text-xs lg:text-base font-semibold z-5 bg-gray-900/75 pl-0.5 pr-1 pb-0.5 rounded-br-lg",
                     class: if !card.card_type.contains(CardType::MONSTER) {"hidden"},
                     {format!("✪ {}", card.level.unwrap_or(0))}
                 }
                 div {
-                    class: "absolute bottom-0 right-0 text-white text-[8px] md:text-sm font-semibold z-5 bg-gray-900/75 pr-0.5 pl-1 pt-0.5 rounded-tl-lg",
+                    class: "absolute bottom-0 right-0 text-white text-[8px] md:text-xs lg:text-base font-semibold z-5 bg-gray-900/75 pr-0.5 pl-1 pt-0.5 rounded-tl-lg",
                     class: if !card.card_type.contains(CardType::MONSTER) {"hidden"},
                     {format!("{} / {}", card.attack.unwrap_or(0), card.defense.unwrap_or(0))}
                 }
